340e9290455bb8d8b35a0f73d6c811834635e754
rajarya199 <aryalrajan078@gmail.com>
2025-05-03 00:11:21 +0545

    upload-i

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { useRef } from "react";
 import "./App.css";
 import img1 from "../public/nature.jpg";
 const App = () => {
+  const [file, setFile] = useState('');
+
   const fileInputRef = useRef();
+  useEffect(()=>{
+    const getImage= async()=>{
+if(file){
+  const data=new FormData();
+  data.append("name",file.name)
+  data.append("file",file);
+   await uploadFile(data)
+}
+    }
+    getImage()
+
+  },[file])
   const onUploadClick = () => {
     fileInputRef.current.click();
   };
+
   return (
     <div className="container">
       <img src={img1} alt="banner" />
@@ -14,7 +29,10 @@ const App = () => {
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
         <button onClick={() => onUploadClick()}>Upload</button>
-        <input type="file" style={{ display: "none" }} ref={fileInputRef} />
+        <input type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+
+        style={{ display: "none" }} ref={fileInputRef} />
       </div>
     </div>
   );
