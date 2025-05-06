93f0b9896ae96ca73d790f620835bcb332add8d4
rajarya199 <aryalrajan078@gmail.com>
2025-05-06 22:30:00 +0545

    downloadimg

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -19,3 +19,17 @@ export const uploadImage=async(request,response)=>{
         return response.status(500).json({message:error.message})
     }
 }
+
+export const downloadImage=async(request,response)=>{
+    try{
+        const file = await File.findById(request.params.fileId);
+        
+        file.downloadCount++;
+        await file.save();
+        response.download(file.path, file.name);
+
+    }
+    catch(error){
+        console.log("Error while downloading image",error.message)
+    }
+}
