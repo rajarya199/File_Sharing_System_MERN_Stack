782c737260b3a9a264a34226ccadcbe6d8d9ab34
rajarya199 <aryalrajan078@gmail.com>
2025-05-06 22:30:26 +0545

    get file

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { uploadImage } from '../controllers/image-controller.js';
 const router= express.Router();
-
+import upload from '../utils/upload.js';
 // router.method('/path',middleware,callback fn)
-router.post('/upload',uploadImage)
+router.post('/upload',upload.single('file'),uploadImage)
+router.get('/file/:fileId',downloadImage)
 export default router;
