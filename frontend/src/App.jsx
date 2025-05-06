2e865940d8ad839eb7bc8161b0f9967ccd1e4189
rajarya199 <aryalrajan078@gmail.com>
2025-05-06 23:10:35 +0545

    file upload,download

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React,{useState,useEffect} from "react";
 import { useRef } from "react";
 import "./App.css";
+import { uploadFile } from "./services/api";
 import img1 from "../public/nature.jpg";
 const App = () => {
   const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
 
   const fileInputRef = useRef();
   useEffect(()=>{
@@ -12,7 +15,8 @@ if(file){
   const data=new FormData();
   data.append("name",file.name)
   data.append("file",file);
-   await uploadFile(data)
+ const response=  await uploadFile(data)
+ setResult(response.path)
 }
     }
     getImage()
@@ -24,7 +28,7 @@ if(file){
 
   return (
     <div className="container">
-      <img src={img1} alt="banner" />
+      <img src='/nature.jpg' alt="banner" />
       <div className="wrapper">
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
@@ -33,6 +37,8 @@ if(file){
                   onChange={(e) => setFile(e.target.files[0])}
 
         style={{ display: "none" }} ref={fileInputRef} />
+                <a href={result} target='_blank'>{result}</a> 
+
       </div>
     </div>
   );
