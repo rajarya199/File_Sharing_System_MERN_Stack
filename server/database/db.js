4d75f1b749f2f8e2faac4c44988b26a445102b3d
rajarya199 <aryalrajan078@gmail.com>
2025-05-06 23:10:16 +0545

    db

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const dbConnection= async()=>{
+    const MONGODB_URI=process.env.MONGO_URI
+    // const MONGODB_URI=`mongodb+srv://user-rajan:raj3user@file-sharings.ux1yj21.mongodb.net/?retryWrites=true&w=majority&appName=file-sharings`
     try{
-       await mongoose.connect(MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+       await mongoose.connect(MONGODB_URI,{useNewUrlParser:true})
         console.log("Connected to database")
     } 
     catch(error){
-        console.log.error("Error while connecting to database",error.message)
+        console.error("Error while connecting to database",error.message)
     }
 }
 export default dbConnection;
