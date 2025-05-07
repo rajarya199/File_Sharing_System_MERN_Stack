8acb1856ab635e3b45426bc869526670b716dd97
rajarya199 <aryalrajan078@gmail.com>
2025-05-07 21:10:30 +0545

    upload file-ii

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL 
 export const uploadFile= async(data)=>{
     try{
      let response=await axios.post(`${API_URL}/upload`,data)
